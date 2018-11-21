01c3c6b2b6183b027899c9fa14bb35f97ba3818c
hexoul <crosien@gmail.com>
2018-11-21 10:16:45 +0900

    test: refactor testunit code

--- cryptocurrency_test.go
+++ cryptocurrency_test.go
@@ -8,108 +8,88 @@ import (
 )
 
 func TestCryptoInfo(t *testing.T) {
-	ret, err := GetInstance().CryptoInfo(&types.Options{
+	if ret, err := GetInstance().CryptoInfo(&types.Options{
 		Symbol: "BTC",
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if ret.CryptoInfo["BTC"].Name != "Bitcoin" {
+	} else if ret.CryptoInfo["BTC"].Name != "Bitcoin" {
 		t.FailNow()
 	}
 }
 
 func TestCryptoMap(t *testing.T) {
-	ret, err := GetInstance().CryptoMap(&types.Options{
+	if ret, err := GetInstance().CryptoMap(&types.Options{
 		Symbol: "BTC",
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if len(ret.CryptoMap) == 0 || ret.CryptoMap[0].Name != "Bitcoin" {
+	} else if len(ret.CryptoMap) == 0 || ret.CryptoMap[0].Name != "Bitcoin" {
 		t.FailNow()
 	}
 }
 
 func TestCryptoListingsLatest(t *testing.T) {
-	listings, err := GetInstance().CryptoListingsLatest(&types.Options{
+	if listings, err := GetInstance().CryptoListingsLatest(&types.Options{
 		Limit: 1,
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if len(listings.CryptoMarket) == 0 {
+	} else if len(listings.CryptoMarket) == 0 {
 		t.FailNow()
-	}
-	if listings.CryptoMarket[0].Name != "Bitcoin" {
+	} else if listings.CryptoMarket[0].Name != "Bitcoin" {
 		t.FailNow()
-	}
-	if listings.CryptoMarket[0].Quote["USD"].Price <= 0 {
+	} else if listings.CryptoMarket[0].Quote["USD"].Price <= 0 {
 		t.FailNow()
 	}
 }
 
 func TestCryptoMarketPairsLatest(t *testing.T) {
-	info, err := GetInstance().CryptoMarketPairsLatest(&types.Options{
+	if info, err := GetInstance().CryptoMarketPairsLatest(&types.Options{
 		Symbol: "BTC",
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if info.Name != "Bitcoin" {
+	} else if info.Name != "Bitcoin" {
 		t.FailNow()
 	}
 }
 
 func TestCryptoOhlcvLatest(t *testing.T) {
-	info, err := GetInstance().CryptoOhlcvLatest(&types.Options{
+	if info, err := GetInstance().CryptoOhlcvLatest(&types.Options{
 		Symbol:  "BTC",
 		Convert: "USD",
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if info.Ohlcv["BTC"].Name != "Bitcoin" {
+	} else if info.Ohlcv["BTC"].Name != "Bitcoin" {
 		t.FailNow()
-	}
-	if info.Ohlcv["BTC"].Quote["USD"] == nil {
+	} else if info.Ohlcv["BTC"].Quote["USD"] == nil {
 		t.FailNow()
 	}
 }
 
 func TestCryptoOhlcvHistorical(t *testing.T) {
-	info, err := GetInstance().CryptoOhlcvHistorical(&types.Options{
+	if info, err := GetInstance().CryptoOhlcvHistorical(&types.Options{
 		Symbol:    "BTC",
 		Convert:   "USD",
 		TimeStart: "2018-11-12",
 		TimeEnd:   "2018-11-13",
 		Interval:  types.IntervalOptions.Hourly,
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if info.Name != "Bitcoin" {
+	} else if info.Name != "Bitcoin" {
 		t.FailNow()
-	}
-	if info.Ohlcv[0].Quote["USD"] == nil {
+	} else if info.Ohlcv[0].Quote["USD"] == nil {
 		t.FailNow()
-	}
-	if info.Ohlcv[0].Quote["USD"].Volume == 0 {
+	} else if info.Ohlcv[0].Quote["USD"].Volume == 0 {
 		t.FailNow()
 	}
 }
 
 func TestCryptoMarketQuotesLatest(t *testing.T) {
-	quotes, err := GetInstance().CryptoMarketQuotesLatest(&types.Options{
+	if quotes, err := GetInstance().CryptoMarketQuotesLatest(&types.Options{
 		Symbol: "BTC,ETH",
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if len(quotes.CryptoMarket) == 0 || len(quotes.CryptoMarket) != 2 {
+	} else if len(quotes.CryptoMarket) == 0 || len(quotes.CryptoMarket) != 2 {
 		t.FailNow()
-	}
-	if quotes.CryptoMarket["BTC"].Name != "Bitcoin" || quotes.CryptoMarket["ETH"].Name != "Ethereum" {
+	} else if quotes.CryptoMarket["BTC"].Name != "Bitcoin" || quotes.CryptoMarket["ETH"].Name != "Ethereum" {
 		t.FailNow()
 	}
 }
--- exchange_test.go
+++ exchange_test.go
@@ -8,70 +8,58 @@ import (
 )
 
 func TestExchangeInfo(t *testing.T) {
-	info, err := GetInstance().ExchangeInfo(&types.Options{
+	if info, err := GetInstance().ExchangeInfo(&types.Options{
 		Slug: "binance",
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if info.ExchangeInfo["binance"].Name != "Binance" {
+	} else if info.ExchangeInfo["binance"].Name != "Binance" {
 		t.FailNow()
 	}
 }
 
 func TestExchangeMap(t *testing.T) {
-	info, err := GetInstance().ExchangeMap(&types.Options{
+	if info, err := GetInstance().ExchangeMap(&types.Options{
 		Slug: "binance",
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if len(info.ExchangeMap) == 0 {
+	} else if len(info.ExchangeMap) == 0 {
 		t.FailNow()
-	}
-	if info.ExchangeMap[0].Name != "Binance" {
+	} else if info.ExchangeMap[0].Name != "Binance" {
 		t.FailNow()
 	}
 }
 
 func TestExchangeListingsLatest(t *testing.T) {
-	info, err := GetInstance().ExchangeListingsLatest(&types.Options{
+	if info, err := GetInstance().ExchangeListingsLatest(&types.Options{
 		Limit: 2,
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if len(info.MarketQuote) < 2 {
+	} else if len(info.MarketQuote) < 2 {
 		t.FailNow()
-	}
-	if info.MarketQuote[0].Name != "Binance" {
+	} else if info.MarketQuote[0].Name != "Binance" {
 		t.FailNow()
 	}
 }
 
 func TestExchangeMarketPairsLatest(t *testing.T) {
-	info, err := GetInstance().ExchangeMarketPairsLatest(&types.Options{
-		Slug: "binance",
-	})
-	if err != nil {
+	if info, err := GetInstance().ExchangeMarketPairsLatest(&types.Options{
+		Slug:  "binance",
+		Limit: 200,
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if info.Name != "Binance" {
+	} else if info.Name != "Binance" {
 		t.FailNow()
 	}
 }
 
 func TestExchangeMarketQuotesLatest(t *testing.T) {
-	info, err := GetInstance().ExchangeMarketQuotesLatest(&types.Options{
+	if info, err := GetInstance().ExchangeMarketQuotesLatest(&types.Options{
 		Slug: "binance",
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
-	}
-	if len(info.MarketQuote) == 0 {
+	} else if len(info.MarketQuote) == 0 {
 		t.FailNow()
-	}
-	if info.MarketQuote["binance"].Name != "Binance" {
+	} else if info.MarketQuote["binance"].Name != "Binance" {
 		t.FailNow()
 	}
 }
